831b89f24dcbdb6e33dd752ec27902797457147e
Hemant <hemantschauhan042@gmail.com>
2025-07-20 11:57:18 +0530

    project complete

--- index.js
+++ index.js
@@ -60,12 +60,19 @@ app.patch("/posts/:id", (req,res) => {
     post.content = newContent;
     console.log(post);
     res.redirect("/posts");
+
 })
 
 app.get("/posts/:id/edit", (req,res) => {
     let {id} = req.params;
     let post = posts.find((p) => id === p.id);
     res.render("edit.ejs", {post});
+})
+
+app.delete("/posts/:id", (req,res) => {
+    let {id} = req.params;
+    posts = posts.filter((p) => id != p.id);
+    res.redirect("/posts");
 })
 
 app.listen(port, (res, req) => {
